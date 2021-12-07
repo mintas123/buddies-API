69b42ac765754637518c9b04f05de6ac27deab9a
mintas123 <52136988+mintas123@users.noreply.github.com>
2021-12-07 17:20:04 +0100

    checkstyle implemented

--- src/main/java/pl/mroz/buddiesapi/BuddiesApiApplication.java
+++ src/main/java/pl/mroz/buddiesapi/BuddiesApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BuddiesApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BuddiesApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BuddiesApiApplication.class, args);
+    }
 
 }
