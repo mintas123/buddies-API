5027be790e09e00de24b241ce3c5c2855df991a1
mintas123 <52136988+mintas123@users.noreply.github.com>
2021-12-08 14:28:33 +0100

    gradle files cleanup, init Rental testing

---
+++ src/test/commons/pl/mroz/buddiesapi/domain/RentalFixtureFactory.groovy
@@ -0,0 +1,10 @@
+package pl.mroz.buddiesapi.domain
+
+import pl.mroz.buddiesapi.domain.rental.Rental
+
+class RentalFixtureFactory {
+    static Rental createValidRandomRental(){
+        //todo when doing the domain
+        return null
+    }
+}
