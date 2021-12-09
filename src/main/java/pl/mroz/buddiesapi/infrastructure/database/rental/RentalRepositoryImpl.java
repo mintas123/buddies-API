2dd357f1d12dbc90985b160891fda3f217f592df
mintas123 <52136988+mintas123@users.noreply.github.com>
2021-12-09 14:57:06 +0100

    All fields in Rental dtos and Entity, more ddd configuration in this domain

--- src/main/java/pl/mroz/buddiesapi/infrastructure/database/rental/RentalRepositoryImpl.java
+++ src/main/java/pl/mroz/buddiesapi/infrastructure/database/rental/RentalRepositoryImpl.java
@@ -1,15 +1,29 @@
 package pl.mroz.buddiesapi.infrastructure.database.rental;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
+import pl.mroz.buddiesapi.domain.rental.Rental;
 import pl.mroz.buddiesapi.domain.rental.RentalRepository;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class RentalRepositoryImpl implements RentalRepository {
 
+    private final RentalJpaRepository jpaRepository;
+
     @Override
     public int someMethod() {
         return 0;
+    }
+
+    public List<Rental> getAll() {
+        return null;
+    }
+
+    interface RentalJpaRepository extends JpaRepository<Rental, Long> {
+
     }
 }
